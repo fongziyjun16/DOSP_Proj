dd2b25b751ee3d72fd679ea0fdd69d7aeb34cab1
fongziyjun16 <fongziyjun16@hotmail.com>
2021-09-16 23:15:20 -0400

    demo for remote communication

---
+++ Proj01/RemoteDemo/System01/Program.fs
@@ -0,0 +1,46 @@
+﻿// System01 simulates a responsor
+open Akka.FSharp
+open Akka.Remote
+open Akka.Configuration
+
+let configuration = ConfigurationFactory.ParseString(@"
+        akka {
+            actor.provider = remote
+            remote {
+                dot-netty.tcp {
+                    port = 9090
+                    hostname = localhost
+                }
+            }
+        }
+    ")
+
+// Akka System
+let sys01 = System.create "sys01" (configuration)
+
+// actor
+let smith = 
+    spawn sys01 "smith"
+        (fun mailbox ->
+            let rec loop() = actor {
+                let! message = mailbox.Receive()
+                let sender = mailbox.Sender()
+                match box message with
+                | :? string ->
+                    printfn "smith receives message: %s" message
+                    sender <! "Hello, I am Smith. Getting your message successfully"
+                    return! loop()
+                | _ -> failwith "unknown error"
+            } 
+            loop())
+
+[<EntryPoint>]
+let main argv =
+    // simulating a responsor, just waiting
+    
+    System.Console.Read() |> ignore
+    0
+
+
+
+
---
+++ Proj01/RemoteDemo/System02/Program.fs
@@ -0,0 +1,35 @@
+﻿// System02 simulates a requester
+open Akka.FSharp
+open Akka.Remote
+open Akka.Configuration
+
+let configuration = ConfigurationFactory.ParseString(@"
+        akka {
+            actor.provider = remote
+            remote {
+                dot-netty.tcp {
+                    port = 9091
+                    hostname = localhost
+                }
+            }
+        }
+    ")
+
+// Akka System
+let sys02 = System.create "sys02" (configuration)
+
+// actor
+let smith = sys02.ActorSelection("akka.tcp://sys01@localhost:9090/user/smith")
+
+[<EntryPoint>]
+let main argv =
+    System.Threading.Thread.Sleep(1000) // wait for System01 starting
+
+    let task = smith <? "Message from Jedi"
+
+    let response = Async.RunSynchronously(task, 3000)
+
+    printfn "response info: %s" (string(response))
+
+    System.Console.Read() |> ignore
+    0 // return an integer exit code
