41e7b3d61d996b21ff003a9091f756a3e9a25a58
fongziyjun16 <fongziyjun16@hotmail.com>
2021-09-28 18:40:28 -0400

    update

--- Proj02/GossipFullNetworkDemo/Actors.fs
+++ Proj02/GossipFullNetworkDemo/Actors.fs
@@ -1,6 +1,7 @@
 ﻿module Actors
 
 open System
+open Akka.Actor
 open Akka.FSharp
 open Akka.Cluster
 open Akka.Cluster.Tools.PublishSubscribe
@@ -10,15 +11,18 @@ open Msgs
 type RecorderActor(numberOfWorkers: int) =
     inherit Actor()
     
+    let mutable stopFlg = false
     let mutable stopTellingCounter = 0
 
     let mutable realTimeStart = new DateTime()
     let mutable realTimeEnd = new DateTime()
     let stopWatch = new Diagnostics.Stopwatch()
 
+    let Cluster = Akka.Cluster.Cluster.Get(Actor.Context.System)
     let mediator = DistributedPubSub.Get(Actor.Context.System).Mediator
 
     override x.PreStart() =
+        Cluster.Subscribe(Actor.Context.Self, ClusterEvent.InitialStateAsEvents, [| typeof<ClusterEvent.IMemberEvent> |])
         mediator <! (new Put(Actor.Context.Self))
 
     override x.OnReceive message =
@@ -27,49 +31,50 @@ type RecorderActor(numberOfWorkers: int) =
             realTimeStart <- DateTime.Now
             stopWatch.Start()
             mediator <! (new Send("/user/worker_" + (Random().Next(numberOfWorkers) + 1).ToString(), new Rumor(), true))
-        | :? EndRumor as msg ->
+        | :? GetRumor as msg ->
             if stopTellingCounter <> numberOfWorkers then
                 stopTellingCounter <- (stopTellingCounter + 1)
             else 
-                realTimeEnd <- DateTime.Now
-                stopWatch.Stop()
-                printfn "stop rumor. real time span: %A ; process run how long: %A" 
-                        (realTimeEnd.Subtract(realTimeStart)) 
-                        stopWatch.Elapsed.Milliseconds
-        | :? RumorCounter as msg ->
-            printfn "from: %s dest: %s counter: %d" msg.FROM msg.DEST msg.COUNTER
+                if stopFlg = false then
+                    stopFlg <- true
+                    realTimeEnd <- DateTime.Now
+                    stopWatch.Stop()
+                    printfn "stop rumor. real time span: %A ; process run how long: %A" 
+                            (realTimeEnd.Subtract(realTimeStart).Milliseconds) 
+                            stopWatch.Elapsed.Milliseconds
+        | :? ClusterEvent.IMemberEvent as msg -> ()
         | _ -> printfn "unknown message"
 
+    override x.PostStop() =
+        Cluster.Unsubscribe(Actor.Context.Self)
 
 type WorkerActor(id: int, numberOfWorkers: int, rumorTimes: int) =
     inherit Actor()
 
     let addrs = new Collections.Generic.List<int>()
     let mutable rumorCounter = 0
-    let mutable flg = false
 
+    let Cluster = Akka.Cluster.Cluster.Get(Actor.Context.System)
     let mediator = DistributedPubSub.Get(Actor.Context.System).Mediator
 
-    override this.PreStart() =
+    override x.PreStart() =
+        Cluster.Subscribe(Actor.Context.Self, ClusterEvent.InitialStateAsEvents, [| typeof<ClusterEvent.IMemberEvent> |])
         mediator <! (new Put(Actor.Context.Self))
         for i in 1 .. numberOfWorkers do
             if i <> id then
                 addrs.Add(i)
 
-    override this.OnReceive message =
+    override x.OnReceive message =
         match box message with
         | :? Rumor as msg ->
-            if rumorCounter <> rumorTimes then
+            if rumorCounter < rumorTimes then
                 rumorCounter <- (rumorCounter + 1)
                 let nextID = (addrs.[Random().Next(numberOfWorkers - 1)])
                 mediator <! (new Send("/user/worker_" + nextID.ToString(), msg, true))
-                mediator <! (new Send("/user/recorder", new RumorCounter("worker_" + id.ToString(), "worker_" + nextID.ToString(), rumorCounter), true))
-            else
-                if flg = false then
-                    flg <- true
-                    mediator <! (new Send("/user/recorder", new EndRumor(), true))
+                mediator <! (new Send("/user/recorder", new GetRumor(), true))
+        | :? ClusterEvent.IMemberEvent as msg -> ()
         | _ -> printfn "unknown message"
 
-    override this.PostStop() =
-        ()
+    override x.PostStop() =
+        Cluster.Unsubscribe(Actor.Context.Self)
         
