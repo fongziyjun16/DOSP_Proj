d286c67ac6ad5922b3879cd7d42ceb53d7ad4368
fongziyjun16 <fongziyjun16@hotmail.com>
2021-10-02 11:36:27 -0400

    Gossip Full Network with using Message NO to record the number of Rounds

---
+++ Proj02/GFNWithMsgNO/Program.fs
@@ -0,0 +1,45 @@
+﻿open System
+
+open Akka.Actor
+open Akka.FSharp
+open Akka.Configuration
+
+open Msgs
+open Actors
+
+// Gossip Full Network
+// Each Message with NO
+
+let numberOfWorkers = 10000
+let times = 10
+let systemName = "GFNSystem"
+
+[<EntryPoint>]
+let main argv =
+    
+    let configuration = ConfigurationFactory.ParseString(@"
+                            akka {
+                                actor.provider = cluster
+                                remote {
+                                    dot-netty.tcp {
+                                        port = 9090
+                                        hostname = localhost
+                                    }
+                                }
+                                cluster {
+                                    seed-nodes = [""akka.tcp://GFNSystem@localhost:9090""]
+                                }
+                            }
+                        ")
+    
+    let GFNSystem = System.create systemName (configuration)
+
+    let recorder = GFNSystem.ActorOf(Props(typeof<RecorderActor>, [| numberOfWorkers :> obj |]), "recorder")
+
+    for i in 1 .. numberOfWorkers do
+        GFNSystem.ActorOf(Props(typeof<FullNetworkWorkerActor>, [| i :> obj; numberOfWorkers :> obj; times :> obj |]), "worker_" + i.ToString()) |> ignore
+    
+    recorder <! (new StartRumor())
+
+    Console.Read() |> ignore
+    0 // return an integer exit code
