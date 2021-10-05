dec837424da8dae8a15fb518c3a26bb180757e71
fongziyjun16 <fongziyjun16@hotmail.com>
2021-10-04 23:37:06 -0400

    update

---
+++ Proj02/GLDemo/Msgs.fs
@@ -0,0 +1,33 @@
+﻿module Msgs
+
+type StartRumor =
+    struct
+    end
+
+type Rumor = 
+    struct
+    end
+
+type GetRumor =
+    struct
+        val ID: int
+        new (id: int) ={
+            ID = id
+        }
+    end
+
+type GetSwitch =
+    struct
+    end
+
+type SetSwitch =
+    struct
+        val SWITCH: bool
+        new (switch: bool) = {
+            SWITCH = switch
+        }
+    end
+
+type AllStop =
+    struct
+    end
