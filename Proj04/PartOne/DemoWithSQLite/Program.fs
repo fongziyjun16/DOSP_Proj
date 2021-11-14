f00c071f8a9138e37b636ab95ce2e49f79078ed5
fongziyjun16 <fongziyjun16@hotmail.com>
2021-11-14 09:36:41 -0500

    update

---
+++ Proj04/PartOne/DemoWithSQLite/Program.fs
@@ -0,0 +1,27 @@
+﻿open System
+open Microsoft.Data.Sqlite
+
+type student = 
+    val UFID: string
+    val NAME: string
+    val address: string
+    new ()
+
+[<EntryPoint>]
+let main argv =
+    
+    use connection = new SqliteConnection("Data Source=exam.db")
+    connection.Open()
+
+    let testSQLCommand = "select * from student"
+    let command = new SqliteCommand(testSQLCommand, connection)
+    
+    let reader  = command.ExecuteReader()
+    while reader.Read() do
+        printfn "%s:%s:%s" 
+            (reader.["ufid"].ToString())
+            (reader.["name"].ToString())
+            (reader.["address"].ToString())
+
+    Console.Read() |> ignore
+    0 // return an integer exit code
